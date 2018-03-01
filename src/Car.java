e12d055f37ae3c1427b12544458163d85ddf4d5e
Foesa <33104551+foesa@users.noreply.github.com>
2018-03-01 18:45:23 +0000

    Car takes trips

--- src/Car.java
+++ src/Car.java
@@ -5,6 +5,7 @@ public class Car {
 	public int destinationColumn;
 	public int carDestinationX;
 	public int carDestinationY;
+	Trip currentTrip;
 	public Car() {
 		carDestinationX = origin;
 		carDestinationY = origin;
@@ -13,6 +14,52 @@ public class Car {
 	public void destination(int x, int y) {
 		this.destinationRow = y;
 		this.destinationColumn = x;
+	}
+	
+	public void changeTrip(Trip carTrip) {
+		this.currentTrip = carTrip;
+		
+	}
+	
+
+	public int getOrigin() {
+		return origin;
+	}
+
+	public void setOrigin(int origin) {
+		this.origin = origin;
+	}
+
+	public int getDestinationRow() {
+		return destinationRow;
+	}
+
+	public void setDestinationRow(int destinationRow) {
+		this.destinationRow = destinationRow;
+	}
+
+	public int getDestinationColumn() {
+		return destinationColumn;
+	}
+
+	public void setDestinationColumn(int destinationColumn) {
+		this.destinationColumn = destinationColumn;
+	}
+
+	public int getCarDestinationX() {
+		return carDestinationX;
+	}
+
+	public void setCarDestinationX(int carDestinationX) {
+		this.carDestinationX = carDestinationX;
+	}
+
+	public int getCarDestinationY() {
+		return carDestinationY;
+	}
+
+	public void setCarDestinationY(int carDestinationY) {
+		this.carDestinationY = carDestinationY;
 	}
 
 }
