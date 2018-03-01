124d3da0ccc51fb4742e2f353d92627120f97150
Kacper <kacper19990@hotmail.com>
2018-03-01 18:41:51 +0000

    Buffer temp

--- src/Self_Driving.java
+++ src/Self_Driving.java
@@ -1,3 +1,16 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class Self_Driving {
-    
+    ArrayList<Trip> trips = new ArrayList<Trip>();
+    try(BufferedReader br = new BufferedReader(new FileReader("src/temp"))){
+        boolean eof = false;
+        while(!eof){
+
+        }
+    }catch(IOException e){
+        e.printStackTrace();
+    }
 }
