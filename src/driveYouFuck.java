3a7e51631078195b0a78319bce39f91c810bb71d
Foesa <33104551+foesa@users.noreply.github.com>
2018-03-01 21:10:37 +0000

    pusherino

--- src/driveYouFuck.java
+++ src/driveYouFuck.java
@@ -2,9 +2,8 @@
 public class driveYouFuck {
 
 	public static void main(String[] args) {
-
 		Self_Driving selfDriving = new Self_Driving();
-		selfDriving.readFile("src");
+		selfDriving.readFile("\\C:\\Users\\legua\\Downloads\\a_example.in");
 		selfDriving.run();
 
 	}
