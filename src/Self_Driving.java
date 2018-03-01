38f0e5d0bdfe294a94557de702ab3a7b8b6869a0
Kacper <kacper19990@hotmail.com>
2018-03-01 19:43:44 +0000

    Read lines

--- src/Self_Driving.java
+++ src/Self_Driving.java
@@ -13,6 +13,12 @@ public class Self_Driving {
     private static int LATEST_FINISH = 5;
 
     public static void Main(String[] args) {
+
+    }
+
+    public void readFile(String source){
+        Trip trip = new Trip();
+        ArrayList<Trip> trips = new ArrayList<Trip>();
         int rows;
         int columns;
         int vehicles;
@@ -20,13 +26,31 @@ public class Self_Driving {
         int bonus;
         int steps;
 
-        ArrayList<Trip> trips = new ArrayList<Trip>();
+
         try (BufferedReader br = new BufferedReader(new FileReader("src/temp"))) {
             String first = br.readLine();
+            String[] initialise = first.split(SEPARATOR);
+            rows = Integer.parseInt(initialise[0]);
+            columns = Integer.parseInt(initialise[1]);
+            vehicles = Integer.parseInt(initialise[2]);
+            rides = Integer.parseInt(initialise[3]);
+            bonus = Integer.parseInt(initialise[4]);
+            steps = Integer.parseInt(initialise[5]);
+
             boolean eof = false;
             while (!eof) {
                 String car = br.readLine();
+                for(int i = 0; i < rides; i++){
+                    String[] tripe = car.split(SEPARATOR);
+                    int startR = Integer.parseInt(tripe[START_ROW]);
+                    int startC = Integer.parseInt(tripe[START_COLUMN]);
+                    int endR = Integer.parseInt(tripe[END_ROW]);
+                    int endC = Integer.parseInt(tripe[END_COLUMN]);
+                    int earlyS = Integer.parseInt(tripe[EARLIEST_START]);
+                    int latestF = Integer.parseInt(tripe[LATEST_FINISH]);
+                    trips.add(new Trip(startR, startC, endR, endC, earlyS, latestF));
 
+                }
             }
         } catch (IOException e) {
             e.printStackTrace();
