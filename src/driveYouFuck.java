3dd74c5c86cedd49455994eef7314bd8d2948627
Foesa <33104551+foesa@users.noreply.github.com>
2018-03-01 21:36:25 +0000

    ignore

--- src/driveYouFuck.java
+++ src/driveYouFuck.java
@@ -3,7 +3,7 @@ public class driveYouFuck {
 
 	public static void main(String[] args) {
 		Self_Driving selfDriving = new Self_Driving();
-		selfDriving.readFile("H:\\Eclipse\\HashCode\\src\\b_should_be_easy.in");
+		selfDriving.readFile("C:\\Users\\legua\\Downloads\\b_should_be_easy.in");
 		selfDriving.run();
 
 	}
