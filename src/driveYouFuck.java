612117e1e71641d4fd26221f55b86bd6df3e95f5
dartse <33062156+dartse@users.noreply.github.com>
2018-03-01 21:27:54 +0000

    df

--- src/driveYouFuck.java
+++ src/driveYouFuck.java
@@ -3,7 +3,7 @@ public class driveYouFuck {
 
 	public static void main(String[] args) {
 		Self_Driving selfDriving = new Self_Driving();
-		selfDriving.readFile("H:\\Eclipse\\HashCode\\src\\b_should_be_easy.in");
+		selfDriving.readFile("H:\\Eclipse\\HashCode\\src\\c_no_hurry.in");
 		selfDriving.run();
 
 	}
